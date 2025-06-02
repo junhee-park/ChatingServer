ad178a58b9d7a844601d22ff4b37e9a8065e7ea4
USER <USER@LAPTOP-NSIBD9JU>
2025-06-02 17:44:08 +0900

    패킷 매니저 생성 재네레이터 프로젝트 생성

---
+++ PacketGenerator/CodeTempletes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketGenerator
+{
+    internal class CodeTempletes
+    {
+        /*
+         * {0} 패킷 핸들러와 패킷 생성 메소드들을 추가하는 부분
+         */
+        public const string PacketManager = @"
+using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using ServerCore;
+
+public class PacketManager
+{{
+    #region Singleton
+    static PacketManager _instance = new PacketManager();
+    public static PacketManager Instance {{ get {{ return _instance; }} }}
+    #endregion
+
+    Dictionary<ushort, Action<Session, IMessage>> _handlers = new Dictionary<ushort, Action<Session, IMessage>> ();
+    Dictionary<ushort, Func<ArraySegment<byte>, IMessage>> _makePacket = new Dictionary<ushort, Func<ArraySegment<byte>, IMessage>>();
+        
+    public PacketManager()
+    {{
+{0}
+    }}
+
+    public T MakePacket<T>(ArraySegment<byte> buffer) where T : IMessage, new()
+    {{
+        T packet = new T();
+        packet.MergeFrom(buffer);
+
+        return packet;
+    }}
+
+    public void InvokePacketHandler(Session session, ArraySegment<byte> buffer)
+    {{
+        ushort size = BitConverter.ToUInt16(buffer.Array, 0);
+        ushort packetId = BitConverter.ToUInt16(buffer.Array, 2);
+
+        ArraySegment<byte> data = new ArraySegment<byte>(buffer.Array, 4, size - 4);
+        bool result = _makePacket.TryGetValue(packetId, out var makePacketFunc);
+        if (!result)
+        {{
+            return;
+        }}
+        IMessage packet = makePacketFunc.Invoke(data);
+
+        result = _handlers.TryGetValue(packetId, out var handler);
+        if (!result)
+        {{
+            return;
+        }}
+        handler?.Invoke(session, packet);
+    }}
+}}
+";
+        /*
+         * {0} Protocol파일에 정의된 메세지 아이디
+         * {1} 핸들러 및 패킷 클래스 이름
+         */
+        public const string PacketHandler =
+@"        _handlers.Add((ushort)MsgId.{0}, PacketHandler.{1}Handler);
+        _makePacket.Add((ushort)MsgId.{0}, MakePacket<{1}>);";
+    }
+}
---
+++ PacketGenerator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace PacketGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../../../../Common/protoc-31.1-win64/bin/protocol.proto";
+            string serverPacketManager;
+            string clientPacketManager;
+            StringBuilder serverHandler = new StringBuilder();
+            StringBuilder clientHandler = new StringBuilder();
+
+            if (args.Length > 0)
+                filePath = args[0];
+
+            var lines = File.ReadLines(filePath);
+            bool isData = false;
+            foreach (var line in lines)
+            {
+                // 메세지 정의가 시작되는 부분까지 건너 뜀
+                if (isData == false)
+                {
+                    if (!line.Contains("MsgId"))
+                        continue;
+                    else
+                    {
+                        isData = true;
+                        continue;
+                    }  
+                }
+
+                if (line.Contains("}") || !(line.Contains("S_") || line.Contains("C_")))
+                    break;
+
+                string packetName = line.Split(" = ")[0].ToLower().Trim();
+                // S_TEST_MESSAGE -> STestMessage
+                StringBuilder nameType1 = new StringBuilder();
+                foreach (var name in packetName.Split('_'))
+                {
+                    if (name.Length == 0)
+                        continue;
+
+                    string temp = name.Replace(name, char.ToUpper(name[0]) + name.Substring(1));
+                    nameType1.Append(temp);
+
+                }
+                // S_TEST_MESSAGE -> S_TestMessage
+                StringBuilder nameType2 = new StringBuilder();
+                nameType2.Append(nameType1);
+                nameType2.Insert(1, '_');
+
+                string handlerCode = string.Format(CodeTempletes.PacketHandler, nameType1, nameType2);
+                if (line.Contains("S_"))
+                {
+                    serverHandler.AppendLine(handlerCode);
+                }
+                else if (line.Contains("C_"))
+                {
+                    clientHandler.AppendLine(handlerCode);
+                }
+            }
+            serverPacketManager = string.Format(CodeTempletes.PacketManager, serverHandler);
+            clientPacketManager = string.Format(CodeTempletes.PacketManager, clientHandler);
+
+            File.WriteAllText("ServerPacketManager.cs", serverPacketManager);
+            File.WriteAllText("ClientPacketManager.cs", clientPacketManager);
+        }
+    }
+}
